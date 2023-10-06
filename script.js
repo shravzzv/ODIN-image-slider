076453f7516157f671fa62aa953223d641f2141e
sai shravan <saishravan384@gmail.com>
2023-10-06 22:35:03 +0530

    feat: Add bottom navigation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const prevEl = document.querySelector('#prev')
 const nextEl = document.querySelector('#next')
 const slidesEl = document.querySelector('#slides')
+const navEl = document.querySelector('.nav')
 const allImages = document.querySelectorAll('#slides img')
 
 let moveWidth = 0
@@ -25,5 +26,17 @@ const handlePrevClick = (e) => {
   }
 }
 
+const handleNavBtnClick = (e) => {
+  const index = Array.from(navEl.children).indexOf(e.currentTarget)
+  moveWidth = index * 500
+  slidesEl.style.translate = `-${moveWidth}px`
+}
+
 nextEl.addEventListener('click', handleNextClick)
 prevEl.addEventListener('click', handlePrevClick)
+
+Array.from(allImages).forEach((image) => {
+  const button = document.createElement('button')
+  navEl.appendChild(button)
+  button.addEventListener('click', handleNavBtnClick)
+})
